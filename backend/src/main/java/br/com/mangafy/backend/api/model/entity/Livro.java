01298f4367982839a8ebdfab63d21bac575f8a38
Frm2003 <felipper475@gmail.com>
2024-12-05 19:45:22 -0300

    Update livro domain

--- backend/src/main/java/br/com/mangafy/backend/api/model/entity/Livro.java
+++ backend/src/main/java/br/com/mangafy/backend/api/model/entity/Livro.java
@@ -9,7 +9,10 @@ public class Livro {
 	private UUID id;
 	private String titulo;
 	private Autor autor;
+	private Editora editora;
 	private int qtdPaginas;
+	private String genero;
+	private String path;
 	
 	public Livro() {
 		this.id = UUID.randomUUID();
--- backend/src/main/java/br/com/mangafy/backend/api/repository/LivroRepository.java
+++ backend/src/main/java/br/com/mangafy/backend/api/repository/LivroRepository.java
@@ -1,5 +1,9 @@
 package br.com.mangafy.backend.api.repository;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.mangafy.backend.api.model.entity.Livro;
@@ -15,7 +19,7 @@ public class LivroRepository {
 	
 	@Transactional
 	public Livro save(Livro livro) {
-		String sql = "INSERT INTO livros (id_livro, titulo, id_autor, qtd_paginas) VALUES (?, ?, ?, ?)";
+		String sql = "INSERT INTO livros (id_livro, titulo, id_autor, qtd_paginas, id_editora, genero, path) VALUES (?, ?, ?, ?, ?, ?, ?)";
 		
 		Query nativeQuery = this.entityManager.createNativeQuery(sql);
 		
@@ -23,9 +27,28 @@ public class LivroRepository {
 		nativeQuery.setParameter(2, livro.getTitulo());
 		nativeQuery.setParameter(3, livro.getAutor().getId());
 		nativeQuery.setParameter(4, livro.getQtdPaginas());
+		nativeQuery.setParameter(5, livro.getEditora().getId());
+		nativeQuery.setParameter(6, livro.getGenero());
+		nativeQuery.setParameter(7, livro.getPath());
 		
 		nativeQuery.executeUpdate();
 		
 		return livro;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Livro> findAllById(UUID id) {
+		String sql = "SELECT * FROM livros WHERE id_autor = ? OR id_editora = ? ";
+
+		Query nativeQuery = this.entityManager.createNativeQuery(sql);
+		nativeQuery.setParameter(1, id);
+		nativeQuery.setParameter(2, id);
+
+		try {
+			List<Livro> livros = nativeQuery.getResultList();
+			return livros;
+		} catch (Exception e) {
+			return Collections.emptyList();
+		}
 	}
 }
